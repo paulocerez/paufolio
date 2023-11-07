1b403748b5e8ede6482e67197cc57af3ccd34f43
paulocerez <paulo.ramirez@web.de>
2023-11-07 13:12:27 +0100

    fix: Loom

--- app/_components/loom.tsx
+++ app/_components/loom.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Loom: React.FC = () => {
   return (
     <iframe
-      src="https://www.loom.com/embed/031d1345fe0545e182b4658d02af48d6?sid=0d7222b3-6baf-4b93-bb2d-beb85e250421?hide_owner=true&hide_share=true&hide_title=true&hideEmbedTopBar=true"
+      src="https://www.loom.com/embed/9b1b6dfa5b7946509a0bfe9c43d97609?sid=3afcaa90-e017-4e8d-8dc5-2073dcfea8a3?hide_owner=true&hide_share=true&hide_title=true&hideEmbedTopBar=true"
       frameBorder="0"
       allowFullScreen
       style={{
