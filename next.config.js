b6efe6d261e3bc5b42e0352cf8147c79bc277a91
paulocerez <paulo.ramirez@web.de>
2025-03-13 21:14:34 +0100

    fix build error, remove next-video

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-const { withNextVideo } = require('next-video/process')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
@@ -8,4 +6,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withNextVideo(nextConfig);
+module.exports = nextConfig;
